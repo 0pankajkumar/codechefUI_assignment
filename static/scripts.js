46afdab3a33a86d8e49616b51066ce45bdeabd8f
Pankaj Kumar <805.bluebell@gmail.com>
2019-07-03 22:57:53 +0530

    just carousal remaining with remaining glitches in mobile view

---
+++ static/scripts.js
@@ -0,0 +1,16 @@
+
+function myFunction1() {
+    document.getElementById("myDIV2").style.display = "none";
+    document.getElementById("myDIV3").style.display = "none";
+    document.getElementById("myDIV1").style.display = "block";
+  }
+  function myFunction2() {
+    document.getElementById("myDIV1").style.display = "none";
+    document.getElementById("myDIV3").style.display = "none";
+    document.getElementById("myDIV2").style.display = "block";
+  }
+  function myFunction3() {
+    document.getElementById("myDIV1").style.display = "none";
+    document.getElementById("myDIV2").style.display = "none";
+    document.getElementById("myDIV3").style.display = "block";
+  }
